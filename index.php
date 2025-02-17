64722a972c73cde8b96303d37a58fe37042f2a9a
emilie-ponce <emilie.ponce@laplateforme.io>
2025-02-17 17:15:02 +0100

    page accueil pratiquement terminée

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Atma:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Sour+Gummy:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -21,6 +21,38 @@
             </ul>
         </nav>
     </header>
+
+<!--PAGE-->
+    <section class="page">
+        <p class="bienvenue">
+            BIENVENUE À L’ANNIVERSAIRE DE BERNADETTE !
+        </p>
+        
+        <div class="biographie">
+            En ce 21 février 2025, nous célébrons l'anniversaire de Bernadette, notre étoile du disco ! Avec son amour pour la musique des années 80 et ses pas de danse enflammés, Bernadatte illumine toutes les soirées. Son énergie contagieuse et sa bonne humeur font d'elle une amie précieuse pour chacun de nous. Que cette journée soit aussi brillante et joyeuse qu'elle !<br><br>Bon anniversaire, Berndatte !
+        </div>
+
+        <div class="deuxcommentaires">
+            <div class="deuxcommentaires2">
+                <p class="premiercommentaire">
+                    "Bernadette, tu es la reine des années 80! Joyeux anniversaire et que la fête commence avec du bon vieux Michael Jackson et Madonna!"
+                </p>
+                <p class="deuxièmecommentaire">
+                    "Qu'importe le temps qui passe, ton cœur est toujours aussi jeune et vibrant que dans les années 80. Joyeux anniversaire, Bernadette !"
+                </p>
+                <p class="troisièmecommentaire">
+                    "Une décennie iconique pour une femme tout aussi iconique. Que cette année soit remplie de bonheur. Joyeux anniversaire, Bernadette !"
+                </p>
+            </div>
+        </div>
+        
+        <div class="envoyermessage">
+            <a class="envoyermessage2" href="">
+                Laissez un message pour Bernadette
+            </a>
+        </div>
+    </section>
+
     
 </body>
 </html>
