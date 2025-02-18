e101416c8012df75025942d3351bb2f17011a08c
emilie-ponce <emilie.ponce@laplateforme.io>
2025-02-18 09:02:08 +0100

    il faut rajouter le footer

--- index.php
+++ index.php
@@ -15,8 +15,8 @@
     <header>
         <nav>
             <ul class="hautdepage">
-                <li><a class="hautdepage3" href="">Connexion</a></li>
                 <li><a class="hautdepage2" href="">BERNADETTE'S BIRTHDAY</a></li>
+                <li><a class="hautdepage3" href="">Connexion</a></li>
                 <li><a class="profil" href=""><img class="profil" src="image/profil.png" alt="logo profil"></a></li>
             </ul>
         </nav>
