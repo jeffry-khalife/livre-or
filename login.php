52ac88f17b6b6a789784cac6cf488017a34ce9f5
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-02-18 11:07:19 +0100

    Ajout des lien github en href dans le footer

--- login.php
+++ login.php
@@ -72,11 +72,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         © Copyright
         <div class="Copyright">
             <p>Magali Vacher
-            <br><a href="#"><img src = "image/githublogo.png" alt="logo github"></a></p>
-            <p>Anna Maras
-            <br><a href="#"><img src = "image/githublogo.png" alt="logo github"></a></p>
+            <br><a href="https://github.com/Vacher-Magali"><img src = "image/githublogo.png" alt="logo github"></a></p>
+            <p>Anna Marras
+            <br><a href="https://github.com/Anna-Marras"><img src = "image/githublogo.png" alt="logo github"></a></p>
             <p>Emilie Ponce
-            <br><a href="#"><img src = "image/githublogo.png" alt="logo github"></a></p>
+            <br><a href="https://github.com/emilie-ponce"><img src = "image/githublogo.png" alt="logo github"></a></p>
             <p>Jeffry KHALIFE
             <br><a href="https://github.com/jeffry-khalife"><img src = "image/githublogo.png" alt="logo github"></a></p>
         </div>
