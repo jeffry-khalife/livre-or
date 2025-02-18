2fcc478c06414d0bead20f0ca469ffdb4a0880f1
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-02-18 12:54:40 +0100

    Amélioration des media queries de la page d'accueil

---
+++ accueil.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Caveat' rel='stylesheet'>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style2.css">
+    <title>Profil</title>
+</head>
+
+<body>
+<nav>
+        <a href="index.php">Bernadette's Birthday</a>
+        <a href="login.php"><img src="image/profil.png" alt="icone profil"></a>
+    </nav>
+
+
+    <div class="content">
+        <div class="container">
+            <section class="page">
+                <p class="bienvenue">
+                            BIENVENUE À L’ANNIVERSAIRE DE BERNADETTE !
+                        </p>
+                        
+                        <div class="biographie">
+                            En ce 21 février 2025, nous célébrons l'anniversaire de Bernadette, notre étoile du disco ! Avec son amour pour la musique des années 80 et ses pas de danse enflammés, Bernadatte illumine toutes les soirées. Son énergie contagieuse et sa bonne humeur font d'elle une amie précieuse pour chacun de nous. Que cette journée soit aussi brillante et joyeuse qu'elle !<br><br>Bon anniversaire, Bernadette !
+                        </div>
+
+                        <div class="deuxcommentaires">
+                            <div class="deuxcommentaires2">
+                                <p class="premiercommentaire">
+                                    "Bernadette, tu es la reine des années 80 ! Joyeux anniversaire et que la fête commence avec du bon vieux Michael Jackson et Madonna !"
+                                </p>
+                                <p class="deuxièmecommentaire">
+                                    "Qu'importe le temps qui passe, ton cœur est toujours aussi jeune et vibrant que dans les années 80. Joyeux anniversaire, Bernadette !"
+                                </p>
+                                <p class="troisièmecommentaire">
+                                    "Une décennie iconique pour une femme tout aussi iconique. Que cette année soit remplie de bonheur. Joyeux anniversaire, Bernadette !"
+                                </p>
+                            </div>
+                        </div>
+                        
+                        <div class="envoyermessage">
+                            <a class="envoyermessage2" href="">
+                                Laissez un message pour Bernadette
+                            </a>
+                        </div>
+            </section>
+        </div>
+    </div>
+
+    <footer>
+        © Copyright
+        <div class="Copyright">
+            <p>Magali Vacher
+            <br><a href="https://github.com/Vacher-Magali"><img src = "image/githublogo.png" alt="logo github"></a></p>
+            <p>Anna Marras
+            <br><a href="https://github.com/Anna-Marras"><img src = "image/githublogo.png" alt="logo github"></a></p>
+            <p>Emilie Ponce
+            <br><a href="https://github.com/emilie-ponce"><img src = "image/githublogo.png" alt="logo github"></a></p>
+            <p>Jeffry KHALIFE
+            <br><a href="https://github.com/jeffry-khalife"><img src = "image/githublogo.png" alt="logo github"></a></p>
+        </div>
+    </footer>
+</body>
+</html>
