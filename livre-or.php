f2ceb9bc1e64c70fe7c5571b6ba978f2138712b2
Vacher-Magali <magali.vacher@laplateforme.io>
2025-02-20 14:07:30 +0100

    modification du flip-container

--- livre-or.php
+++ livre-or.php
@@ -1,20 +1,20 @@
 <?php
 class connexion {
-    private $pdo;
+  private $pdo;
 
-    public function __construct($host, $livreor, $user, $pass){
-        try {
-            $this->pdo = new PDO("mysql:host=$host;dbname=$livreor;charset=utf8", $user, $pass);
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }
-        catch (PDOException $e) {
-            die("Erreur de connexion : " . $e->getMessage());
-        }
-    }
+  public function __construct($host, $livreor, $user, $pass){
+      try {
+          $this->pdo = new PDO("mysql:host=$host;dbname=$livreor;charset=utf8", $user, $pass);
+          $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      }
+      catch (PDOException $e) {
+          die("Erreur de connexion : " . $e->getMessage());
+      }
+  }
 
-    public function getPdo(){
-        return $this->pdo;
-    }
+  public function getPdo(){
+      return $this->pdo;
+  }
 }
 
 /* Classecomment */
